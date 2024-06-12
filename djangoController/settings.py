fea727166697a06c65d283a7b5fe2cc321034fcd
veyhn <109932151+veyhn@users.noreply.github.com>
2024-06-12 19:46:39 -0400

    User profile made. Node next

--- djangoController/settings.py
+++ djangoController/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- djangoController/urls.py
+++ djangoController/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # include urls from different apps
+from userprofile import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('create/', views.create_user, name = 'create_user'),
+    
+   
 ]
